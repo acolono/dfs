54fb6b4fec3fd7655f469d57d3052f2860499266
Florian Fida <f+git@fida.biz>
2018-07-13 21:44:36 +0200

    move app

--- DigitalFailState.Web/Controllers/HomeController.cs
+++ DigitalFailState.Web/Controllers/HomeController.cs
@@ -10,15 +10,13 @@ namespace DigitalFailState.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => RedirectToAction("App");
+
+        public IActionResult App() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Error() {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }

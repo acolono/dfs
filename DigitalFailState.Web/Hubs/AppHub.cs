a72b11ae49d28188b2f4a6fd84c8cbc7edacbafa
Florian Fida <f+git@fida.biz>
2018-07-13 18:58:15 +0200

    remove default scaffolding/template, signalr demo

---
+++ DigitalFailState.Web/Hubs/AppHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DigitalFailState.Web.Hubs
+{
+
+    public class AppHub : Hub
+    {
+        public string GetMessage() {
+            return $"Hello {this.Context.ConnectionId}";
+        }
+
+        public override Task OnConnectedAsync() {
+            Clients.All.SendCoreAsync("NewConnection", new object[]{Context.ConnectionId});
+            return base.OnConnectedAsync();
+        }
+    }
+}
--- DigitalFailState.Web/Startup.cs
+++ DigitalFailState.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DigitalFailState.Web.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -22,41 +23,23 @@ namespace DigitalFailState.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
+        public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
+            app.UseSignalR(c => {
+                c.MapHub<AppHub>("/appHub");
+            });
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

d9f061991180d68d5e36c44d08ec78cfcffac1f5
Florian Fida <f+git@fida.biz>
2018-07-15 23:23:20 +0200

    mqtt score

--- DigitalFailState.Web/Controllers/HomeController.cs
+++ DigitalFailState.Web/Controllers/HomeController.cs
@@ -27,6 +27,8 @@ namespace DigitalFailState.Web.Controllers
             return View();
         }
 
+        public IActionResult MqttScore() => View();
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});

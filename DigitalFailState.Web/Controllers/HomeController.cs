42dcdc3dcae7d10fcf3d5992a198671eec9cbf3c
Florian Fida <f+git@fida.biz>
2018-07-15 21:50:13 +0200

    legacy score

--- DigitalFailState.Web/Controllers/HomeController.cs
+++ DigitalFailState.Web/Controllers/HomeController.cs
@@ -5,15 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DigitalFailState.Web.Models;
+using DigitalFailState.Web.Services;
 
 namespace DigitalFailState.Web.Controllers
 {
     public class HomeController : Controller {
+        private readonly ScoreProvider _scoreProvider;
+
+        public HomeController(ScoreProvider scoreProvider) {
+            _scoreProvider = scoreProvider;
+        }
+
         public IActionResult Index() => View();
 
         public IActionResult App() => View();
 
         public IActionResult Score() => View();
+
+        public IActionResult LegacyScore() {
+            ViewBag.Score = _scoreProvider.GetScore();
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {

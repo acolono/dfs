c8d7298fddb2a2bebe2852b0e3a7056edf128ee0
Florian Fida <f+git@fida.biz>
2018-07-16 21:33:14 +0200

    dispose client after usage, reuse options

--- DigitalFailState.Web/Services/MqttService.cs
+++ DigitalFailState.Web/Services/MqttService.cs
@@ -11,9 +11,16 @@ namespace DigitalFailState.Web.Services
     public class MqttService
     {
         private readonly ILogger<MqttService> _logger;
+        private readonly IMqttClientOptions _options;
 
         public MqttService(ILogger<MqttService> logger) {
             _logger = logger;
+            _options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString().Replace("-",""))
+                .WithTcpServer("iot.eclipse.org")
+                .WithTls()
+                .WithCleanSession()
+                .Build();
         }
 
         public async Task UpdateScoreSilentAsync(long score) {
@@ -21,27 +28,23 @@ namespace DigitalFailState.Web.Services
                 await UpdateScoreAsync(score);
             }
             catch (Exception e) {
-                _logger.LogError(e,"mqtt error");
+                _logger.LogError(e, "mqtt error");
             }
         }
 
         private async Task UpdateScoreAsync(long score) {
-            var options = new MqttClientOptionsBuilder()
-                .WithClientId(Guid.NewGuid().ToString().Replace("-",""))
-                .WithTcpServer("iot.eclipse.org")
-                .WithTls()
-                .WithCleanSession()
-                .Build();
             var factory = new MqttFactory();
-            var client = factory.CreateMqttClient();
-            await client.ConnectAsync(options);
-            var message = new MqttApplicationMessageBuilder()
-                .WithTopic("aes-2018/digital-fail-state/score")
-                .WithPayload(score.ToString())
-                .WithExactlyOnceQoS()
-                .WithRetainFlag()
-                .Build();
-            await client.PublishAsync(message);
+            using (var client = factory.CreateMqttClient()) {
+                await client.ConnectAsync(_options);
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic("aes-2018/digital-fail-state/score")
+                    .WithPayload(score.ToString())
+                    .WithExactlyOnceQoS()
+                    .WithRetainFlag()
+                    .Build();
+                await client.PublishAsync(message);
+                await client.DisconnectAsync();    
+            }
         }
     }
 }
